5879764d34b17245d50e0fd369a7e8a0ae61e786
Per Noalt <per.noalt@truesec.se>
2023-05-17 14:54:34 +0200

    initial .net core 2.1 + 3.1 projects

---
+++ src/ConsoleAppNetCore21/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppNetCore21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/ConsoleAppNetCore31/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppNetCore31
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
